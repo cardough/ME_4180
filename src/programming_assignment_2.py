8cc2052d4c6eab0738dd4cccbb0a496adc9a4648
Carter Dougherty <carterd@iastate.edu>
2026-03-31 12:15:23 -0500

    added a new JointSlider class to control a specific joint

--- src/programming_assignment_2.py
+++ src/programming_assignment_2.py
@@ -7,8 +7,9 @@ plt.show = lambda *args, **kwargs: None     # prevents pyplot another window fro
 
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QWidget,
-    QVBoxLayout, QHBoxLayout,
-    QSlider, QPushButton, QLineEdit, QLabel
+    QVBoxLayout, QHBoxLayout, QFrame,
+    QSlider, QPushButton, QLineEdit, QLabel,
+    QFormLayout
 )
 from PySide6.QtCore import Qt
 
@@ -53,7 +54,7 @@ class RobotCanvas(FigureCanvas):
         angle_ranges = np.array(angle_ranges).T
 
         # use forward kinematics to find trajectory shape
-        x, y, z = [0 for i in range(number_steps)], [0 for i in range(number_steps)], [0 for i in range(number_steps)]
+        x, y, z = [0 for _ in range(number_steps)], [0 for _ in range(number_steps)], [0 for _ in range(number_steps)]
         for i, q in enumerate(angle_ranges):
             pos = self.robot.fkine(q).t
             x[i] = pos[0]
@@ -74,6 +75,47 @@ class RobotCanvas(FigureCanvas):
         self.update_trajectory(number_steps)
         
 
+class JointSlider(QFrame):
+    def __init__(self, joint_index):
+        super().__init__()
+        self.setAttribute(Qt.WA_StyledBackground, True)
+        self.setStyleSheet("""
+            QFrame {
+                background-color: #262626;
+                border-radius: 5px;
+            }
+        """)
+        
+        main_layout = QVBoxLayout(self)
+        
+
+        text_entry_layout = QFormLayout()
+        self.joint_val = QLineEdit("0.0")
+        text_entry_layout.addRow(f"Joint {joint_index} (rad):", self.joint_val)
+        main_layout.addLayout(text_entry_layout)
+
+        self.joint_slider = QSlider(Qt.Horizontal)
+        self.joint_slider.setMinimum(-314)   # ~ -pi
+        self.joint_slider.setMaximum(314)    # ~ pi
+        self.joint_slider.setValue(0)
+        self.joint_slider.valueChanged.connect(self.sync_text_box)
+        main_layout.addWidget(self.joint_slider)
+        
+    
+    def sync_text_box(self, value):
+        rad = value / 100.0
+        self.joint_val.setText(f"{rad:.3f}")
+
+    def get_joint_val(self):
+        try:
+            # retrieve and convert angle value
+            val = float(self.joint_val.text())
+        except:
+            raise ValueError
+        
+        # clamp to valid range
+        return max(-np.pi, min(np.pi, val))
+
 
 
 class ControlPanel(QWidget):
@@ -82,79 +124,28 @@ class ControlPanel(QWidget):
         self.canvas = canvas
 
         layout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         layout.addWidget(QLabel("Number of Timesteps:"))
         self.n_steps = QLineEdit("10")
         layout.addWidget(self.n_steps)
 
-        # joint 1 slider
-        layout.addWidget(QLabel("Joint 1 (radians):"))
-        self.joint_one = QLineEdit("0.0")
-        layout.addWidget(self.joint_one)
+        self.joint_controls = [None for _ in range(3)]
+        for i in range(3):
+            self.joint_controls[i] = JointSlider(i)
+            layout.addWidget(self.joint_controls[i])
 
-        self.joint_one_slider = QSlider(Qt.Horizontal)
-        self.joint_one_slider.setMinimum(-314)   # ~ -pi
-        self.joint_one_slider.setMaximum(314)    # ~ pi
-        self.joint_one_slider.setValue(0)
-        layout.addWidget(self.joint_one_slider)
-
-        #joint 2 slider
-        layout.addWidget(QLabel("Joint 2 (radians):"))
-        self.joint_two = QLineEdit("0.0")
-        layout.addWidget(self.joint_two)
-
-        self.joint_two_slider = QSlider(Qt.Horizontal)
-        self.joint_two_slider.setMinimum(-314)   # ~ -pi
-        self.joint_two_slider.setMaximum(314)    # ~ pi
-        self.joint_two_slider.setValue(0)
-        layout.addWidget(self.joint_two_slider)
-
-        # joint 3 slider
-        layout.addWidget(QLabel("Joint 3 (radians):"))
-        self.joint_three = QLineEdit("0.0")
-        layout.addWidget(self.joint_three)
-
-        self.joint_three_slider = QSlider(Qt.Horizontal)
-        self.joint_three_slider.setMinimum(-314)   # ~ -pi
-        self.joint_three_slider.setMaximum(314)    # ~ pi
-        self.joint_three_slider.setValue(0)
-        layout.addWidget(self.joint_three_slider)
 
         self.button = QPushButton("Update")
         layout.addWidget(self.button)
 
-        self.joint_one_slider.valueChanged.connect(self.sync_one)
-        self.joint_two_slider.valueChanged.connect(self.sync_two)
-        self.joint_three_slider.valueChanged.connect(self.sync_three)
-
         # make the update button re-render the canvas
         self.button.clicked.connect(self.apply_update)
 
-    def sync_one(self, value):
-        rad = value / 100.0
-        self.joint_one.setText(f"{rad:.3f}")
-
-    def sync_two(self, value):
-        rad = value / 100.0
-        self.joint_two.setText(f"{rad:.3f}")
-
-    def sync_three(self, value):
-        rad = value / 100.0
-        self.joint_three.setText(f"{rad:.3f}")
-
     def apply_update(self):
-        try:
-            q1 = float(self.joint_one.text())
-            q2 = float(self.joint_two.text())
-            q3 = float(self.joint_three.text())
-        except ValueError:
-            return
-
-        # Clamp to slider range for consistency
-        q1 = max(-np.pi, min(np.pi, q1))
-        q2 = max(-np.pi, min(np.pi, q2))
-        q3 = max(-np.pi, min(np.pi, q3))
-        self.canvas.update_joints([q1, q2, q3, 0], int(self.n_steps.text()))
+        joint_values = [self.joint_controls[i].get_joint_val() for i in range(3)]
+        joint_values.append(0)
+        self.canvas.update_joints(joint_values, int(self.n_steps.text()))
 
 
 class MainWindow(QMainWindow):
